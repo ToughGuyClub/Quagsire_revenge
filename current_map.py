f92784589a5b86ce9191478483bf48f3687af59a
ToughGuyClub(laptab) <gyudong6185@gmail.com>
2025-11-09 10:19:59 +0900

    맵 추가 위한 클래스 생성

--- current_map.py
+++ current_map.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+width, height = 1400, 800
 class CurrentMap:
     def __init__(self):
         self.current_map = 0
@@ -11,3 +12,22 @@ class CurrentMap:
         pass
     def draw(self):
         pass
+
+class MapConnection:
+    def __init__(self):
+        # current_map_id : {"direction": (next_map_id, spawn_x, spawn_y)}
+        self.links = {
+            2: {  # Town Map
+                "east":  (4, 100, 400),   # 오른쪽 끝 → 숲맵
+            },
+            3: {
+
+            },
+            4: {
+
+            }
+        }
+
+    def get_transition(self, current_map_id, direction):
+        """다음 맵 정보 반환: (next_map_id, spawn_x, spawn_y)"""
+        return self.links.get(current_map_id, {}).get(direction, None)
